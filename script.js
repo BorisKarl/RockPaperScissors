b8ab401416a2df41716e3235e18741aac89d402c
BorisKarl <>
2021-12-16 00:10:10 +0100

    Komplett neu

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
+const buttons = document.querySelectorAll('input');
 
-let wins = 0;
-let loss = 0;
-function game(){
+
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
@@ -9,56 +8,59 @@ function game(){
       }
     
     function computerPlay(){
-        let r = "stein";
-        let p = "papier";
-        let s = "schere";
+        let r = "Stein";
+        let p = "Papier";
+        let s = "Schere";
         let answer = [r, p, s];
         let i = getRandomInt(0,3);
         return answer[i];
     }
-    
-    let b = computerPlay();
-    
-    let playerSelection = prompt("Stein, Schere oder Papier?").toLowerCase();
-    alert("Computer: " + b);
-    let a = playerSelection;
-    if(a == b) {
-        alert("Unentschieden!")
-    }else if(a == "stein" && b == "schere") {
-        alert("Gewonnen! Stein schlägt Schere!");
-        wins += 1;
-    }else if(a == "papier" && b == "stein") {
-        alert("Gewonnen! Papier schlägt Stein!");
-        wins+= 1;
-    }else if(a == "schere" && b == "papier") {
-        alert("Gewonnen! Schere schlägt Papier!");
-        wins+= 1;
-    }else if(a == "stein" && b == "papier") {
-        alert("Gewonnen! Papier schlägt stein!");
-        loss+= 1;
-    }else if(a =="papier" && b == "schere") {
-        alert("Gewonnen! Schere schlägt Papier!");
-        loss+= 1;
-    }else if(a =="schere" && b =="stein") {
-        alert("Gewonnen! Stein schlägt Schere!")
-        loss+= 1;
-    }else{
-        prompt("Bitte gib Papier, Stein oder Schere ein!");
-       
+    function disableButtons() {
+        buttons.forEach(elem => {
+            elem.disabled = true
+        })
     }
-   alert("Spielstand: Spieler " + wins + " Computer " + loss);
-   //alert("Spielerwahl: " + a);
-   
+  
+let playerScore = 0;
+    let computerScore = 0;
+    let result = "";
+
+
+function game(playerSelection){
+    
+    const a = playerSelection;
+    const b = computerPlay();
+    
+    if(
+             (a == "Stein" && b == "Schere") ||
+             (a == "Papier" && b == "Stein") ||
+             (a == "Schere" && b == "Papier")
+            ) 
+            {    
+                playerScore += 1;
+                result = (`Gewonnen! ${a} schlägt ${b} ! <br><br>Spieler: ${playerScore} <br>Computer: ${computerScore}`)
+                if (playerScore == 5) {
+                    result += '<br><br>Du hast gewonnen! Lade die Seite neu, um nochmal zu spielen'
+                    disableButtons();
+                }
+    }else if (a == b){
+        result = (`Unentschieden!<br><br>Spieler: ${playerScore}<br>Computer: ${computerScore}`);       
+  }else{
+        computerScore += 1;
+        result = (`Verloren! ${a} schlägt ${b} ! <br><br>Spieler: ${playerScore} <br>Computer: ${computerScore}` )
+        if (computerScore == 5) {
+            result += '<br><br>Der Computer hat gewonnen! Lade die Seite neu, um nochmal zu spielen'
+            disableButtons();
     }
+ 
+}
+document.getElementById('result').innerHTML = result
+return
+}
+    
+buttons.forEach(button => {
+        button.addEventListener('click', function(){
+            game(button.value)
+        })
+    })
 
-    for(i = 0; i < 5; i++){
-    game();
-    };
-    if (wins > loss) {
-        alert("Du hast mit " + wins + " Siegen gewonnen!", "Stein, Schere, Papier");
-
-    }else if(loss > wins){
-        alert("Du hast mit " + wins + " zu " + loss + " verloren.");
-    }else{
-        alert("Unentschieden!");
-    };
